0fb32c77f4b421aa266419664145918ee6dd337b
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2016-03-01 22:54:43 +0530

    File controller added

---
+++ ConfigManager/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ConfigManager.Controllers
+{
+    public class FileController : ApiController
+    {
+        public HttpResponseMessage Get(string name)
+        {
+            string contents;
+
+            using (var streamReader = new StreamReader(name))
+            {
+                contents = streamReader.ReadToEnd();
+            }
+            return this.Request.CreateResponse(HttpStatusCode.OK, contents);
+        }
+    }
+}
---
+++ ConfigManager/Controllers/FileInfoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Configuration;
+using ConfigManager.Models;
+
+namespace ConfigManager.Controllers
+{
+    public class FileInfoController : ApiController
+    {
+        public HttpResponseMessage Get(string name)
+        {
+            var files = Directory.GetFiles(ConfigurationManager.AppSettings[name]);
+            var fileModelList = new List<FileModel>();
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                fileModelList.Add(new FileModel
+                {
+                    Name = fileInfo.Name,
+                    Path = fileInfo.FullName,
+                    LastModified = fileInfo.LastWriteTime
+                });
+
+            }
+            return this.Request.CreateResponse(HttpStatusCode.OK, fileModelList);
+        }
+    }
+}
